9db53eda8371f16e1ec874e96721b752e4ce85de
HaroonAzizi <haroonazizi15@gmail.com>
2025-03-12 20:36:43 +0430

    changed xyluZ to xyluz

--- app/icon.js
+++ app/icon.js
@@ -1,17 +1,14 @@
 import { ImageResponse } from "next/og";
 import { generateSVGFavicon } from "./favicon";
 
-// Route segment config
 export const runtime = "edge";
 
-// Image metadata
 export const size = {
   width: 32,
   height: 32,
 };
 export const contentType = "image/png";
 
-// Image generation
 export default function Icon() {
   return new ImageResponse(
     (
